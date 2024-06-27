1fb5e33d8160a294ad43d7fe5d8735f9e4e7b285
antagonist13 <1703shakayaroslaw@gmail.com>
2024-06-27 18:48:51 +0300

    TS App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,70 +1,72 @@
-// import css from './App.module.css'
-// import SearchBar from '../SearchBar/SearchBar'
-// import ImageGallery from '../ImageGallery/ImageGallery'
-// import Loader from '../Loader/Loader'
-// import ErrorMessage from '../ErrorMessage/ErrorMessage'
-// import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn'
-// import ImageModal from '../ImageModal/ImageModal'
-// import { getImages } from '/src/images-api.js'
-// import { useEffect, useState } from 'react'
+import css from './App.module.css'
+import SearchBar from '../SearchBar/SearchBar'
+import ImageGallery from '../ImageGallery/ImageGallery'
+import Loader from '../Loader/Loader'
+import ErrorMessage from '../ErrorMessage/ErrorMessage'
+import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn'
+import ImageModal from '../ImageModal/ImageModal'
+import { getImages } from '../../images-api'
+import { useEffect, useState } from 'react'
+import { ObjectType } from '../../types'
 
-// export default function App() {
-//     const [query, setQuery] = useState <string>()
-//     const [isMounted, setIsMounted] = useState<boolean>(false);
-//     const [photos, setPhotos] = useState([]);
-//     const [page, setPage] = useState<number>(1);
-//     const [totalPages, setTotalPages] = useState<number>();
-//     const [isLoading, setIsLoading] = useState<boolean>(false);
-//     const [isError, setIsError] = useState<boolean>(false);
-//     const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
-//     const [modalImgURL, setmodalImgURL] = useState<string>('');
 
-//   useEffect(() => {
-//     async function fetchImages() {
-//         try {
-//         setIsLoading(true);
-//         setIsError(false);
-//         const list = await getImages(query, page);
-//             setPhotos((prevState) => [...prevState, ...list.data.results]);
-//             setTotalPages(list.data.total_pages)
-//       } catch (error) {
-//             console.log(error);
-//             setIsError(true);
-//       } finally {
-//             setIsLoading(false);
-//       }
-//     }
-//     if (isMounted) {
-//         fetchImages();
-//     } else {
-//       setIsMounted(true);
-//     }
-//   // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, [query, page]);
+export default function App() {
+    const [query, setQuery] = useState <string>('')
+    const [isMounted, setIsMounted] = useState<boolean>(false);
+    const [photos, setPhotos] = useState<ObjectType[]>([]);
+    const [page, setPage] = useState<number>(1);
+    const [totalPages, setTotalPages] = useState<number>(0);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isError, setIsError] = useState<boolean>(false);
+    const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+    const [modalImgURL, setmodalImgURL] = useState<string>('');
+
+  useEffect(() => {
+    async function fetchImages() {
+        try {
+        setIsLoading(true);
+        setIsError(false);
+        const list: any = await getImages(query, page);
+            setPhotos((prevState: ObjectType[]) => [...prevState, ...list.data.results]);
+            setTotalPages(list.data.total_pages)
+      } catch (error) {
+            console.log(error);
+            setIsError(true);
+      } finally {
+            setIsLoading(false);
+      }
+    }
+    if (isMounted) {
+        fetchImages();
+    } else {
+      setIsMounted(true);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query, page]);
     
-//       const handleSearch = async (value) => {
-//     setQuery(value);
-//     setPage(1);
-//     setPhotos([]);
-//     }
-//       const handleLoadMore = async () => {
-//     setPage(page + 1);
-//     }
-//     const handleModalOpen = () => {
-//         setModalIsOpen(true);
-//     }
-//     const handleModalClose = () => {
-//         setModalIsOpen(false);
-//     }
-//     const handleModalImg = (url) => {
-//         setmodalImgURL(url);
-//     }
-//     return <div className={css.container}>
-//         <SearchBar setSearchData={handleSearch} />
-//         {isError && <ErrorMessage/>}
-//         {photos.length > 0 && <ImageGallery photos={photos} modalOpen={handleModalOpen} setModalUrl={handleModalImg} />}
-//         {isLoading && <Loader />}
-//         {photos.length > 0 && !isLoading && page < totalPages && <LoadMoreBtn loadMore={handleLoadMore} />}
-//         <ImageModal modalIsOpen={modalIsOpen} onRequestClose={handleModalClose} modalUrl={modalImgURL} />
-// </div>
-// }
+      const handleSearch = async (value : string) => {
+    setQuery(value);
+    setPage(1);
+    setPhotos([]);
+    }
+      const handleLoadMore = async () => {
+    setPage(page + 1);
+    }
+    const handleModalOpen = () => {
+        setModalIsOpen(true);
+    }
+    const handleModalClose = () => {
+        setModalIsOpen(false);
+    }
+    const handleModalImg = (url: string) => {
+        setmodalImgURL(url);
+    }
+    return <div className={css.container}>
+        <SearchBar setSearchData={handleSearch} />
+        {isError && <ErrorMessage/>}
+        {photos.length > 0 && <ImageGallery photos={photos} modalOpen={handleModalOpen} setModalUrl={handleModalImg} />}
+        {isLoading && <Loader />}
+        {photos.length > 0 && !isLoading && page < totalPages && <LoadMoreBtn loadMore={handleLoadMore} />}
+        <ImageModal modalIsOpen={modalIsOpen} onRequestClose={handleModalClose} modalUrl={modalImgURL} />
+</div>
+}
