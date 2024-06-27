7aa1599484c99a1a1aac3f6ccb54f53ca00006a3
antagonist13 <1703shakayaroslaw@gmail.com>
2024-06-27 18:09:04 +0300

    1

--- src/images-api.ts
+++ src/images-api.ts
@@ -1,18 +1,18 @@
-// import axios from "axios";
+import axios from "axios";
 
-// axios.defaults.baseURL = "https://api.unsplash.com";
-// const ACESS_KEY = 'd-L6wwOxVoHwVTK9MU_8WHUKXbZoMbaGJ5WvRLBqdN8'
+axios.defaults.baseURL = "https://api.unsplash.com";
+const ACESS_KEY = 'd-L6wwOxVoHwVTK9MU_8WHUKXbZoMbaGJ5WvRLBqdN8'
 
-// export const getImages = async ( query, page) => {
-//   const response = await axios.get("/search/photos", {
-//       params: {
-//           per_page: 20,
-//           page: page,
-//             query: query
-//       },
-//       headers: {
-//             Authorization: `Client-ID ${ACESS_KEY}`,
-//           },
-//   });
-//   return response;
-// };
+export const getImages = async <T>( query: string, page: number): Promise<T> => {
+  const response: T= await axios.get("/search/photos", {
+      params: {
+          per_page: 20,
+          page: page,
+            query: query
+      },
+      headers: {
+            Authorization: `Client-ID ${ACESS_KEY}`,
+          },
+  });
+  return response;
+};
